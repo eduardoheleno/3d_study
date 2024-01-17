c505e0c1be7db9fef4d8defa9a71d834ad6d67b7
Eduardo Gomes Heleno <eduardo.gheleno@gmail.com>
2024-01-17 16:54:04 -0300

    start some refactoring

--- demo/src/main.rs
+++ demo/src/main.rs
@@ -6,7 +6,8 @@ use raylib::prelude::*;
 struct Vec3D {
     x: f32,
     y: f32,
-    z: f32
+    z: f32,
+    w: f32
 }
 
 struct Triangle {
@@ -18,13 +19,173 @@ struct Mesh {
     tris: Vec<Triangle>
 }
 
-struct MatProj {
+struct Mat4x4 {
     m: [[f32; 4]; 4]
 }
 
 impl Vec3D {
-    fn new(x: f32, y: f32, z: f32) -> Self {
-        Self { x, y, z }
+    fn new(x: f32, y: f32, z: f32, w: f32) -> Self {
+        Self { x, y, z, w }
+    }
+
+    fn vector_add(v1: &Self, v2: &Self) -> Self {
+        Self::new(
+            v1.x + v2.x,
+            v1.y + v2.y,
+            v1.z + v2.z,
+            0.0
+        )
+    }
+
+    fn vector_sub(v1: &Self, v2: &Self) -> Self {
+        Self::new(
+            v1.x - v2.x,
+            v1.y - v2.y,
+            v1.z - v2.z,
+            0.0
+        )
+    }
+
+    fn vector_mul(v1: &Self, k: f32) -> Self {
+        Self::new(
+            v1.x * k,
+            v1.y * k,
+            v1.z * k,
+            0.0
+        )
+    }
+
+    fn vector_div(v1: &Self, k: f32) -> Self {
+        Self::new(
+            v1.x / k,
+            v1.y / k,
+            v1.z / k,
+            0.0
+        )
+    }
+
+    fn vector_dotproduct(v1: &Self, v2: &Self) -> f32 {
+        v1.x*v2.x + v1.y*v2.y + v1.z*v2.z
+    }
+
+    fn vector_crossproduct(v1: &Self, v2: &Self) -> Self {
+        let x = v1.y * v2.z - v1.z * v2.y;
+        let y = v1.z * v2.x - v1.x * v2.z;
+        let z = v1.x * v2.y - v1.y * v2.x;
+
+        Self::new(x, y, z, 0.0)
+    }
+
+    fn vector_length(v: &Self) -> f32 {
+        Self::vector_dotproduct(v, v).sqrt()
+    }
+
+    fn vector_normalize(v: &Self) -> Self {
+        let l = Self::vector_length(v);
+        Self::new(
+            v.x / l,
+            v.y / l,
+            v.z / l,
+            0.0
+        )
+    }
+
+    fn matrix_multiply_vector(m: &Mat4x4, i: &Self) -> Self {
+        let x = i.x * m.m[0][0] + i.y * m.m[1][0] + i.z * m.m[2][0] + i.w * m.m[3][0];
+        let y = i.x * m.m[0][1] + i.y * m.m[1][1] + i.z * m.m[2][1] + i.w * m.m[3][1];
+        let z = i.x * m.m[0][2] + i.y * m.m[1][2] + i.z * m.m[2][2] + i.w * m.m[3][2];
+        let w = i.x * m.m[0][3] + i.y * m.m[1][3] + i.z * m.m[2][3] + i.w * m.m[3][3];
+
+        Self::new(x, y, z, w)
+    }
+}
+
+impl Mat4x4 {
+    fn matrix_make_identity() -> Self {
+        let mut matrix = Mat4x4::default();
+        matrix.m[0][0] = 1.0;
+        matrix.m[1][1] = 1.0;
+        matrix.m[2][2] = 1.0;
+        matrix.m[3][3] = 1.0;
+
+        matrix
+    }
+
+    fn matrix_make_rotation_x(f_angle_rad: f32) -> Self {
+        let mut matrix = Mat4x4::default();
+        matrix.m[0][0] = 1.0;
+        matrix.m[1][1] = f_angle_rad.cos();
+        matrix.m[1][2] = f_angle_rad.sin();
+        matrix.m[2][1] = -f_angle_rad.sin();
+        matrix.m[2][2] = f_angle_rad.cos();
+        matrix.m[3][3] = 1.0;
+
+        matrix
+    }
+
+    fn matrix_make_rotation_y(f_angle_rad: f32) -> Self {
+        let mut matrix = Mat4x4::default();
+        matrix.m[0][0] = f_angle_rad.cos();
+        matrix.m[0][2] = f_angle_rad.sin();
+        matrix.m[2][0] = -f_angle_rad.sin();
+        matrix.m[1][1] = 1.0;
+        matrix.m[2][2] = f_angle_rad.cos();
+        matrix.m[3][3] = 1.0;
+
+        matrix
+    }
+
+    fn matrix_make_rotation_z(f_angle_rad: f32) -> Self {
+        let mut matrix = Mat4x4::default();
+        matrix.m[0][0] = f_angle_rad.cos();
+        matrix.m[0][1] = f_angle_rad.sin();
+        matrix.m[1][0] = -f_angle_rad.sin();
+        matrix.m[1][1] = f_angle_rad.cos();
+        matrix.m[2][2] = 1.0;
+        matrix.m[3][3] = 1.0;
+
+        matrix
+    }
+
+    fn matrix_make_translation(x: f32, y: f32, z: f32) -> Self {
+        let mut matrix = Mat4x4::default();
+        matrix.m[0][0] = 1.0;
+        matrix.m[1][1] = 1.0;
+        matrix.m[2][2] = 1.0;
+        matrix.m[3][3] = 1.0;
+        matrix.m[3][0] = x;
+        matrix.m[3][1] = y;
+        matrix.m[3][2] = z;
+
+        matrix
+    }
+
+    fn matrix_make_projection(f_fov_degrees: f32, f_aspect_ratio: f32, f_near: f32, f_far: f32) -> Self {
+        let f_fov_rad = 1.0 / (f_fov_degrees * 0.5 / 180.0 * 3.14159).tan();
+        let mut matrix = Mat4x4::default();
+        matrix.m[0][0] = f_aspect_ratio * f_fov_rad;
+        matrix.m[1][1] = f_fov_rad;
+        matrix.m[2][2] = f_far / (f_far - f_near);
+        matrix.m[3][2] = (-f_far * f_near) / (f_far - f_near);
+        matrix.m[2][3] = 1.0;
+        matrix.m[3][3] = 0.0;
+
+        matrix
+    }
+
+    fn matrix_multiply_matrix(m1: &Self, m2: &Self) -> Self {
+        let mut matrix = Mat4x4::default();
+        for c in 0..4 {
+            for r in 0..4 {
+                matrix.m[r][c] =
+                    m1.m[r][0] * m2.m[0][c] +
+                    m1.m[r][1] * m2.m[1][c] +
+                    m1.m[r][2] * m2.m[2][c] +
+                    m1.m[r][3] * m2.m[3][c];
+            }
+        }
+
+        matrix
     }
 }
 
@@ -59,7 +220,7 @@ impl Mesh {
                     let y = line_vec.get(2).unwrap().to_owned().parse::<f32>().unwrap();
                     let z = line_vec.get(3).unwrap().to_owned().parse::<f32>().unwrap();
 
-                    let v = Vec3D::new(x, y, z);
+                    let v = Vec3D::new(x, y, z, 0.0);
                     verts.push(v);
                 },
                 'f' => {
@@ -85,7 +246,7 @@ impl Mesh {
     }
 }
 
-impl Default for MatProj {
+impl Default for Mat4x4 {
     fn default() -> Self {
         Self {
             m: [[0.0; 4]; 4]
@@ -95,7 +256,7 @@ impl Default for MatProj {
 
 impl Default for Vec3D {
     fn default() -> Self {
-        Self { x: 0.0, y: 0.0, z: 0.0 }
+        Self { x: 0.0, y: 0.0, z: 0.0, w: 0.0 }
     }
 }
 
@@ -198,7 +359,7 @@ fn main() {
     // ];
 
     let mut mesh = Mesh::default();
-    let mut mat_proj: MatProj = Default::default();
+    let mut mat_proj: Mat4x4 = Default::default();
 
     mesh.load_from_object_file("VideoShip.obj");
     // let mesh = Mesh::new(triangle_vec);
@@ -219,8 +380,8 @@ fn main() {
     mat_proj.m[2][3] = 1.0;
     mat_proj.m[3][3] = 0.0;
 
-    let mut mat_rot_z: MatProj = Default::default();
-    let mut mat_rot_x: MatProj = Default::default();
+    let mut mat_rot_z: Mat4x4 = Default::default();
+    let mut mat_rot_x: Mat4x4 = Default::default();
 
     let (mut rl, thread) = raylib::init()
         .size(800, 800)
@@ -248,6 +409,8 @@ fn main() {
         mat_rot_x.m[2][1] = -(f_theta * 0.5).sin() as f32;
         mat_rot_x.m[2][2] = (f_theta * 0.5).cos() as f32;
         mat_rot_x.m[3][3] = 1.0;
+
+        let mut vec_triangles_to_raster: Vec<Triangle> = vec![];
 
         for tri in &mesh.tris {
             // Draw triangles
@@ -300,7 +463,7 @@ fn main() {
                 normal.z * (tri_translated.p[0].z - v_camera.z)
             ) < 0.0 {
                 // Illumination
-                let mut light_direction = Vec3D { x: 0.0, y: 0.0, z: -1.0 };
+                let mut light_direction = Vec3D { x: 0.0, y: 0.0, z: -1.0, w: 0.0 };
                 let l = (light_direction.x*light_direction.x + light_direction.y*light_direction.y + light_direction.z*light_direction.z).sqrt();
                 light_direction.x /= l; light_direction.y /= l; light_direction.z /= l;
 
@@ -327,22 +490,36 @@ fn main() {
                 tri_projected.p[2].x *= 0.5 * 800.0;
                 tri_projected.p[2].y *= 0.5 * 800.0;
 
-                draw_triangle(
-                    &mut d,
-                    tri_projected.p[0].x,
-                    tri_projected.p[0].y,
-                    tri_projected.p[1].x,
-                    tri_projected.p[1].y,
-                    tri_projected.p[2].x,
-                    tri_projected.p[2].y,
-                    tri_projected.color
-                );
+                // Store triangle for sorting
+                vec_triangles_to_raster.push(tri_projected);
             }
+        }
+
+        // Sort triangles from back to front
+        vec_triangles_to_raster.sort_by(|t1, t2| {
+            let z1 = (t1.p[0].z + t1.p[1].z + t1.p[2].z) / 3.0;
+            let z2 = (t2.p[0].z + t2.p[1].z + t2.p[2].z) / 3.0;
+
+            z2.partial_cmp(&z1).unwrap()
+        });
+
+        for tri_projected in vec_triangles_to_raster {
+            // Rasterize triangle
+            draw_triangle(
+                &mut d,
+                tri_projected.p[0].x,
+                tri_projected.p[0].y,
+                tri_projected.p[1].x,
+                tri_projected.p[1].y,
+                tri_projected.p[2].x,
+                tri_projected.p[2].y,
+                tri_projected.color
+            );
         }
     }
 }
 
-fn multiply_matrix_vector(input: &Vec3D, output: &mut Vec3D, mat_proj: &MatProj) {
+fn multiply_matrix_vector(input: &Vec3D, output: &mut Vec3D, mat_proj: &Mat4x4) {
     output.x = input.x * mat_proj.m[0][0] + input.y * mat_proj.m[1][0] + input.z * mat_proj.m[2][0] + mat_proj.m[3][0];
     output.y = input.x * mat_proj.m[0][1] + input.y * mat_proj.m[1][1] + input.z * mat_proj.m[2][1] + mat_proj.m[3][1];
     output.z = input.x * mat_proj.m[0][2] + input.y * mat_proj.m[1][2] + input.z * mat_proj.m[2][2] + mat_proj.m[3][2];
@@ -384,9 +561,9 @@ fn draw_triangle(d: &mut RaylibDrawHandle, x1: f32, y1: f32, x2: f32, y2: f32, x
 
             d.draw_triangle(t1, t2, t3, c);
 
-            d.draw_line(x1 as i32, y1 as i32, x2 as i32, y2 as i32, Color::BLACK);
-            d.draw_line(x2 as i32, y2 as i32, x3 as i32, y3 as i32, Color::BLACK);
-            d.draw_line(x3 as i32, y3 as i32, x1 as i32, y1 as i32, Color::BLACK);
+            // d.draw_line(x1 as i32, y1 as i32, x2 as i32, y2 as i32, Color::BLACK);
+            // d.draw_line(x2 as i32, y2 as i32, x3 as i32, y3 as i32, Color::BLACK);
+            // d.draw_line(x3 as i32, y3 as i32, x1 as i32, y1 as i32, Color::BLACK);
         },
         None => {
             eprintln!("Triangle doesn't have a color value.");
